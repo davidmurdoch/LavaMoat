a553b16552b3622b2ac89d9786f1fb0dafd57dcd
Christopher Hiller <boneskull@boneskull.com>
2023-11-17 14:26:24 -0800

    chore: wip

--- packages/core/src/generatePolicy.js
+++ packages/core/src/generatePolicy.js
@@ -12,6 +12,7 @@ const {
     mapToObj,
     reduceToTopmostApiCallsFromStrings,
   },
+  inspectEsmImports,
 } = require('lavamoat-tofu')
 const { mergePolicy } = require('./mergePolicy')
 
@@ -232,21 +233,36 @@ function createModuleInspector(opts = {}) {
     const namesForBuiltins = Object.entries(moduleRecord.importMap)
       .filter(([, resolvedName]) => isBuiltin(resolvedName))
       .map(([requestedName]) => requestedName)
-    const { cjsImports: moduleBuiltins } = inspectImports(ast, namesForBuiltins)
-    if (!moduleBuiltins.length) {
+    const { esmImports: esmModuleBuiltins } = inspectEsmImports(
+      ast,
+      namesForBuiltins
+    )
+    const { cjsImports: cjsModuleBuiltins } = inspectImports(
+      ast,
+      namesForBuiltins
+    )
+    if (cjsModuleBuiltins.length + esmModuleBuiltins.length === 0) {
       return
     }
     // add debug info
     if (includeDebugInfo) {
       const moduleDebug = debugInfo[moduleRecord.specifier]
-      moduleDebug.builtin = moduleBuiltins
+      moduleDebug.builtin = [
+        ...new Set([...esmModuleBuiltins, ...cjsModuleBuiltins]),
+      ]
     }
     // aggregate package builtins
     if (!packageToBuiltinImports.has(packageName)) {
       packageToBuiltinImports.set(packageName, [])
     }
     let packageBuiltins = packageToBuiltinImports.get(packageName)
-    packageBuiltins = [...packageBuiltins, ...moduleBuiltins]
+    packageBuiltins = [
+      ...new Set([
+        ...packageBuiltins,
+        ...cjsModuleBuiltins,
+        ...esmModuleBuiltins,
+      ]),
+    ]
     packageToBuiltinImports.set(packageName, packageBuiltins)
   }
 
